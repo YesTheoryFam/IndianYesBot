179c4f3ee616d9cc8ce2c8ab1d57bf95054da676
Mohammed Farish <farishmusthafa@gmail.com>
2020-08-31 19:30:42 +0530

    timeout command added

--- src/events/commands.js
+++ src/events/commands.js
@@ -12,11 +12,73 @@ module.exports = bot => {
 
     const serverLogs = message.guild.channels.cache.get('747121287381516399');
     const betaTestChannel = message.guild.channels.cache.get('746755589895487488');
+    const timeOutChannel = message.guild.channels.cache.get('749988359471890563');
     const archiveCategory = '747111680168820766';
 
         
         let args = message.content.substring(PREFIX.length).split(" ");
         switch(args[0]) {
+
+          case 'timeout':
+    
+            if (message.member.hasPermission('ADMINISTRATOR')) {
+                var person = message.mentions.members.first();
+                if(!person) return message.channel.send("Please specify a valid user.").then(m => m.delete({timeout: 5000}))
+                  .then(message.delete({timeout: 5000}).catch(err => console.log(err)));
+        
+                if(person.id === bot.user.id) 
+                  return message.react('👎')
+                  .then(message.delete({timeout: 5000}).catch(err => console.log(err)));
+        
+                if(person.id === message.author.id) 
+                    return message.react('👎')
+                    .then(message.delete({timeout: 5000}).catch(err => console.log(err)));
+        
+                if(person.hasPermission('MANAGE_NICKNAMES'))
+                    return message.react('👎').then(message.delete({timeout: 5000}).catch(err => console.log(err)));
+        
+                var timeOutRole = person.guild.roles.cache.get("749267183015559219");
+        
+                person.roles.add(timeOutRole);
+                message.react('👍');
+                serverLogs.send("<@" + message.author.id + "> just put <@" + person + "> on Time Out from the <#"+ message.channel.id + "> channel.");
+                message.delete({timeout: 5000}).catch(err => console.log(err));
+        
+            };
+        
+            break;
+        
+            case 'permit':
+        
+            if (message.channel == timeOutChannel) {
+        
+                if (message.member.hasPermission('ADMINISTRATOR')) {
+                    var person = message.mentions.members.first();
+                    if(!person) return message.channel.send("Please specify a valid user.").then(m => m.delete({timeout: 5000}))
+                      .then(message.delete({timeout: 5000}).catch(err => console.log(err)));
+            
+                    if(person.id === bot.user.id) 
+                      return message.react('👎')
+                      .then(message.delete({timeout: 5000}).catch(err => console.log(err)));
+            
+                    if(person.id === message.author.id) 
+                        return message.react('👎')
+                        .then(message.delete({timeout: 5000}).catch(err => console.log(err)));
+            
+                    if(person.hasPermission('MANAGE_NICKNAMES'))
+                        return message.react('👎').then(message.delete({timeout: 5000}).catch(err => console.log(err)));
+            
+                    var timeOutRole = person.guild.roles.cache.get("749267183015559219");
+            
+                    person.roles.remove(timeOutRole);
+                    message.react('👍');
+                    serverLogs.send("<@" + message.author.id + "> just took out <@" + person + "> from Time Out");
+                    message.delete({timeout: 5000}).catch(err => console.log(err));
+            
+                };
+            };
+        
+            break; 
         
             case 'ping':
             if (message.channel == betaTestChannel) {
