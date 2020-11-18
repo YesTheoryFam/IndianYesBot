27cc5c447641ffce2b3769c876efb0a206a6f265
Mohammed Farish <farishmusthafa@gmail.com>
2020-11-19 04:26:11 +0530

    minor update

--- src/database/events/activityGroups.js
+++ src/database/events/activityGroups.js
@@ -1,5 +1,8 @@
 module.exports = (bot) => {
   const activityGroupSchema = require("../Schemas/activityGroupSchema");
+  const serverSchema = require('../Schemas/serverSchema')
+
+  const serverRoles = require('../../collections/Roles/Roles.json')
 
   bot.on('presenceUpdate', async (oldPresence, newPresence) => {
     if (newPresence.user.bot) return;
@@ -7,29 +10,90 @@ module.exports = (bot) => {
 
     const activities = newPresence.activities
     const [Activity] = newPresence.activities;
-    if (!Activity) return;
+    if (Activity) {
 
-    activities.forEach(async (eachActivity) => {
-      if (eachActivity.name === 'Custom Status') return;
+      activities.forEach(async (eachActivity) => {
+        if (eachActivity.name === 'Custom Status') return;
 
-      const appName = eachActivity.name
+        const appName = eachActivity.name
 
-      await activityGroupSchema.findOneAndUpdate(
-        {
-          appName
-        },
-        {
-          $addToSet: {
-            userIDs: newPresence.member.id,
+        await activityGroupSchema.findOneAndUpdate(
+          {
+            appName
           },
-        },
-        {
-          upsert: true,
+          {
+            $addToSet: {
+              userIDs: newPresence.member.id,
+            },
+          },
+          {
+            upsert: true,
+          }
+        );
+
+      })
+
+    }
+
+    if (newPresence.member.roles.cache.has(serverRoles.YesFamMember)) {
+
+      const userOnline = async () => {
+
+        await serverSchema.findOneAndUpdate({
+          _id: newPresence.guild.id
+        }, {
+          serverName: newPresence.guild.name,
+          $addToSet: {
+            activeUsers: newPresence.userID
+          }
+        }, {
+          upsert: true
+        })
+
+      }
+
+      const userOffline = async () => {
+
+        await serverSchema.findOneAndUpdate({
+          _id: newPresence.guild.id
+        }, {
+          serverName: newPresence.guild.name,
+          $pull: {
+            activeUsers: newPresence.userID
+          }
+        }, {
+          upsert: true
+        })
+
+      }
+
+      if (oldPresence) {
+
+        if (newPresence.status === 'offline' ||
+          newPresence.status === 'invisible') {
+          console.log(newPresence.member.displayName, 'went offline')
+          userOffline();
         }
-      );
 
-    })
+        if (oldPresence.status === 'online' ||
+          oldPresence.status === 'dnd' ||
+          oldPresence.status === 'idle') {
+          return;
+        }
 
+        if (newPresence.status === 'online' ||
+          newPresence.status === 'dnd' ||
+          newPresence.status === 'idle') {
+          console.log(newPresence.member.displayName, 'is online')
+          userOnline();
+        }
+
+      } else {
+        console.log(newPresence.member.displayName, 'is also online')
+        userOnline();
+      }
+
+    }
 
   })
 
