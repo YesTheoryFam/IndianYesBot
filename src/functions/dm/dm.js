50bfd4f6baa59ea328b746b33773c20c18e737da
Mohammed Farish <farishmusthafa@gmail.com>
2021-01-01 14:43:13 +0530

    minor fix

--- src/functions/dm/dm.js
+++ src/functions/dm/dm.js
@@ -1,5 +1,5 @@
 
-module.exports = (message, server, dmParent, existingDM, dmChannelSchema) => {
+module.exports = async (message, server, dmParent, existingDM, dmChannelSchema) => {
     if (message.author.bot) return;
 
     const checkIfExisting = server.members.fetch(message.author.id);
